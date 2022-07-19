bb731c5dd4e7e2f79f35e4f4eaf9779b94145d3b
wzd1145 <3266955168@qq.com>
2022-07-19 22:09:20 +0800

    Table表格

--- component/src/main.js
+++ component/src/main.js
@@ -3,7 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 import './styles/icon/iconfont.css'
+
+import request from '@/utils/request'
+Vue.use(ElementUI)
+
+Vue.prototype.$axios = request
 
 Vue.config.productionTip = false
 
--- component/src/views/Home.vue
+++ component/src/views/Home.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="home">
-    <yangButton>默认按钮</yangButton>
-    <yangButton type="primary"  >成功按钮</yangButton>
-    <yangButton type="danger" >危险按钮</yangButton>
-    <yangButton type="warning" >警告按钮</yangButton>
+    <yang-table init-request :format="formatData" @onLoad="onLoad"  :column="column" index checkbox :data="data_1" :params="params_1" url="/name/" method="post">
+      <template v-slot:operation="slot">
+        <yang-button type="primary" @click="handleEdit(slot.data)">编辑</yang-button>
+        <yang-button type="danger" @click="handleDelete(slot.data)">删除</yang-button>
+        <yang-button type="success" @click="handleEdit(slot.data)">编辑</yang-button>
+        <yang-button type="warning" @click="handleDelete(slot.data)">删除</yang-button>
+      </template>
+    </yang-table>
   </div>
 </template>
 
@@ -13,19 +17,40 @@ export default {
   name: 'Home',
   data () {
     return {
-      flag: false
+      column: [
+        { label: '姓名', prop: 'name' },
+        { label: '性别', prop: 'gender' },
+        { label: '创建时间', prop: 'create_date' },
+        { label: '操作', type: 'slot', slot_name: 'operation', prop: 'operation' }
+      ],
+      data_1: {
+        name: 'jack'
+      },
+      params_1: {
+        name: 'rose'
+      }
     }
   },
   components: {
-    yangButton: () => import('../components/button/index.vue')
+    yangButton: () => import('../components/button/index.vue'),
+    yangTable: () => import('../components/table/index.vue')
   },
   methods: {
-    handleSubmit () {
-      this.flag = true
-      setTimeout(() => {
-        console.log('123')
-        this.flag = false
-      }, 3000)
+    handleEdit (row) {
+      console.log(row)
+    },
+    handleDelete (row) {
+      console.log(row)
+    },
+    onLoad (data) {
+      console.log(data)
+    },
+    formatData (data) {
+      const tableData = data.data
+      tableData.forEach(item => {
+        item.gender = item.gender === '男' ? 1 : 0
+      })
+      return tableData
     }
   }
 }
